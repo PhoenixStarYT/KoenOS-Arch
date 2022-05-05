f29c7037fedf3c78b309158b58812485ba6b3fb1
Koen Kist (PhoenixStarYT) <koenkist18@gmail.com>
2022-05-05 22:10:32 +0200

    SDDM + dotconfig

--- arch-koenos.sh
+++ arch-koenos.sh
@@ -1,7 +1,15 @@
 #!/bin/bash
 
+#Install Paru AUR Helper
+sudo pacman --noconfirm -S --needed base-devel
+git clone https://aur.archlinux.org/paru.git
+cd paru
+makepkg -si
+
 # Update and install base Packages
-sudo pacman -Syu 
+sudo pacman --noconfirm -Syu 
+sudo pacman --noconfirm -S neofetch
+sudo pacman --noconfirm -S htop
 sudo pacman --noconfirm -S awesome
 sudo pacman --noconfirm -S git
 sudo pacman --noconfirm -S kitty
@@ -11,13 +19,12 @@ sudo pacman --noconfirm -S variety
 sudo pacman --noconfirm -S lxappearance
 sudo pacman --noconfirm -S pulseaudio
 sudo pacman --noconfirm -S pavucontrol
-
-#Install Paru AUR Helper
-sudo pacman --noconfirm -S --needed base-devel
-git clone https://aur.archlinux.org/paru.git
-cd paru
-makepkg -si
+sudo pacman --noconfirm -S sddm
 
 #Theming & Fonts
 paru --noconfirm -S xfce-theme-greybird
-#Setup all config files
+mv ~/koenos-arch/Fonts/* ~/.fonts
+mv ~/koenos-arch/dotconfig/* ~/.config
+rm .bashrc
+mv ~/koenos-arch/.bashrc ~/
+
