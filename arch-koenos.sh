fc521f81a6ce93823af444a439811915c63bf3c5
Koen Kist (PhoenixStarYT) <koenkist18@gmail.com>
2022-05-16 15:31:31 +0200

    Update arch-koenos.sh

--- arch-koenos.sh
+++ arch-koenos.sh
@@ -1,18 +1,37 @@
 #!/bin/bash
 
-#Install Paru AUR Helper
+#Welcome
+echo -ne "
+██████╗ ██╗  ██╗ ██████╗ ███████╗███╗   ██╗██╗██╗  ██╗███████╗████████╗ █████╗ ██████╗ ██╗   ██╗████████╗
+██╔══██╗██║  ██║██╔═══██╗██╔════╝████╗  ██║██║╚██╗██╔╝██╔════╝╚══██╔══╝██╔══██╗██╔══██╗╚██╗ ██╔╝╚══██╔══╝
+██████╔╝███████║██║   ██║█████╗  ██╔██╗ ██║██║ ╚███╔╝ ███████╗   ██║   ███████║██████╔╝ ╚████╔╝    ██║
+██╔═══╝ ██╔══██║██║   ██║██╔══╝  ██║╚██╗██║██║ ██╔██╗ ╚════██║   ██║   ██╔══██║██╔══██╗  ╚██╔╝     ██║
+██║     ██║  ██║╚██████╔╝███████╗██║ ╚████║██║██╔╝ ██╗███████║   ██║   ██║  ██║██║  ██║   ██║      ██║
+╚═╝     ╚═╝  ╚═╝ ╚═════╝ ╚══════╝╚═╝  ╚═══╝╚═╝╚═╝  ╚═╝╚══════╝   ╚═╝   ╚═╝  ╚═╝╚═╝  ╚═╝   ╚═╝      ╚═╝
+
+██╗  ██╗ ██████╗ ███████╗███╗   ██╗ ██████╗ ███████╗
+██║ ██╔╝██╔═══██╗██╔════╝████╗  ██║██╔═══██╗██╔════╝
+█████╔╝ ██║   ██║█████╗  ██╔██╗ ██║██║   ██║███████╗
+██╔═██╗ ██║   ██║██╔══╝  ██║╚██╗██║██║   ██║╚════██║
+██║  ██╗╚██████╔╝███████╗██║ ╚████║╚██████╔╝███████║
+╚═╝  ╚═╝ ╚═════╝ ╚══════╝╚═╝  ╚═══╝ ╚═════╝ ╚══════╝
+"
+
+#Paru AUR Helper
 sudo pacman --noconfirm -S --needed base-devel
 git clone https://aur.archlinux.org/paru.git
 cd paru
 makepkg -si
 
-# Update and install base Packages
+#Update and install base Packages
 sudo pacman --noconfirm -Syu 
 sudo pacman --noconfirm -S neofetch
 sudo pacman --noconfirm -S htop
 sudo pacman --noconfirm -S awesome
 sudo pacman --noconfirm -S git
 sudo pacman --noconfirm -S kitty
+
+#Install Desktop apps
 sudo pacman --noconfirm -S thunar
 sudo pacman --noconfirm -S nitrogen
 sudo pacman --noconfirm -S variety
@@ -22,11 +41,11 @@ sudo pacman --noconfirm -S pavucontrol
 sudo pacman --noconfirm -S sddm
 sudo pacman --noconfirm -S rofi
 
+#Setup SDDM
+systemctl enable sddm
+
 #Theming & Fonts
 paru --noconfirm -S xfce-theme-greybird
 mv ~/koenos-arch/fonts/* ~/.fonts
 mv ~/koenos-arch/dotconfig/* ~/.config
-mv ~/koenos-arch/.bashrc ~/
-
-#Setup SDDM
-systemctl enable sddm
+mv ~/koenos-arch/.bashrc
